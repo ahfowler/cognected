a1952954a8409d3b47020141504432f298eb9233
Electrixz <nate.troksa@gmail.com>
2021-02-13 18:38:03 -0700

    trying to implement ajax

--- src/components/UserSettings.vue
+++ src/components/UserSettings.vue
@@ -3,12 +3,30 @@
         <p style="font:">CognectEd User Settings</p>
         <div class="input-group">
             <label id="label">Canvas URL</label>
-            <input placeholder="Institution Canvas URL...  (i.e https://asu.instructure.com/ )" style="width:500px" id="CanvasUrl" v-model="canvasURL"> 
+            <input placeholder="Institution Canvas URL...  (i.e https://asu.instructure.com/ )" style="width:500px" id="CanvasUrl" v-model="canvasURL" @keyup="PopulateCourses" @paste="PopulateCourses"> 
         </div>
 
         <div class="input-group">
             <label id="label">Token</label>
-            <input placeholder="User Token..." id="Token" v-model="token"/>
+            <input placeholder="User Token..." id="Token" v-model="token" @keyup="PopulateCourses" @paste="PopulateCourses"/>
+        </div>
+
+        <div class="dropdown">
+          <div class="dropdown-area">
+            <div
+              id="dropdown-list"
+              v-for="course in courses"
+              :key="course.name"
+            >
+              <input
+                name="courseList"
+                type="radio"
+                :id="course.name"
+                :value="course.name"
+              />
+              <label :for="course.name">{{ course.name }}</label>
+            </div>
+          </div>
         </div>
 
         <a class="myButton" @click="CancelClick" style="float:left;margin-left:50px">Cancel</a>
@@ -18,27 +36,63 @@
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
-    name: "UserSettings",
-    props: {
-      canvasURL: String,
-      token: String,
+  name: "UserSettings",
+  props: {
+    canvasURL: String,
+    token: String,
+    course: String,
+  },
+  data() {
+      return{
+          message: '',
+          courses: [
+            
+        ],
+      };
+  },
+  methods: {
+    CancelClick(){
+      console.log('Cancel Click');
+      this.$emit('clicked', 'Canceled');
     },
-    data() {
-        return{
-            message: '',
-        }
+    ApplyClick(){
+      console.log('Apply Click');
+      this.$emit('clicked', [this.canvasURL, this.token, ]);
     },
-    methods: {
-      CancelClick(){
-        console.log('Cancel Click');
-        this.$emit('clicked', 'Canceled');
-      },
-      ApplyClick(){
-        console.log('Apply Click');
-        this.$emit('clicked', [this.canvasURL, this.token]);
+    PopulateCourses(){
+      if(this.canvasURL.includes("instructure.com") && this.token.length > 60){
+        var corsAnywhere = "https://salty-atoll-62320.herokuapp.com/"; //NEEDED TO CREATE A 'PROPER' CORS API CALL
+        var enrollmentTypes = ["TeacherEnrollment","TaEnrollment","ObserverEnrollment", "DesignerEnrollment"];
+
+        //get course data for each enrollment type
+        enrollmentTypes.forEach((item) => {
+          $.ajax({
+            url:corsAnywhere + this.canvasURL +"/api/v1/courses?enrollment_role=" + item,
+            datatype: "json",
+            method: "GET",
+            headers: {
+              "x-requested-with": "xhr",
+              Authorization: "Bearer " + this.token,
+            },
+            success: function (jsondata) {
+              if (jsondata.length != 0) {
+                console.log(jsondata)
+                this.courses.concat(jsondata)
+              }
+            },
+            error: function (xhr) {
+              console.log(xhr.responseText);
+            },
+          });
+        });
+
+        console.log("Getting Classes")
       }
-    }
+    },
+  }
 }
 </script>
 
@@ -109,4 +163,54 @@ input {
 	position:relative;
 	top:1px;
 }
+
+.dropdown {
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.dropdown input {
+  border: 1px solid #e5e5e5;
+  border-radius: 1px;
+  height: 30px;
+  width: 95%;
+  padding: 5px 10px 5px 10px;
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 15px;
+  line-height: 15px;
+}
+
+::placeholder {
+  color: #e5e5e5;
+}
+
+.dropdown-area {
+  border: 1px solid #e5e5e5;
+  border-radius: 1px;
+  max-height: 200px;
+  overflow-y: scroll;
+  width: 100%;
+  position: relative;
+  top: -1px;
+}
+
+#dropdown-list {
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 15px;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+}
+
+#dropdown-list input {
+  width: 8%;
+  margin: 10px 15px 10px 10px;
+}
+
 </style>
